34af26fedfa483e3ace3d9497a79dea984eba21d
astur <astur@yandex.ru>
2018-08-11 08:07:09 +0300

    scrape with minimum delay

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const {concurrency, httpOptions} = require('./conf');
+const {concurrency, httpOptions, minDelay} = require('./conf');
 const lavine = require('lavine');
+const unquick = require('unquick');
 const scrape = require('./scrape');
 const {onStart, onFinish, onSuccess, onError} = require('./handle');
 
 (async () => {
-    const getWorker = () => () => scrape(httpOptions).then(onSuccess, onError);
+    const worker = unquick(
+        options => scrape(options).then(onSuccess, onError),
+        minDelay || 0
+    );
+    const getWorker = () => () => worker(httpOptions);
     await onStart();
     await lavine(getWorker, concurrency);
     await onFinish();
