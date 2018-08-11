02b71d589ce029c9a69a0426d6cff4116ffb2260
astur <astur@yandex.ru>
2018-08-11 07:25:39 +0300

    scrape with handlers

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const {concurrency, httpOptions} = require('./conf');
 const lavine = require('lavine');
 const scrape = require('./scrape');
+const {onStart, onFinish, onSuccess, onError} = require('./handle');
 
 (async () => {
-    const getWorker = () => () => scrape(httpOptions);
+    const getWorker = () => () => scrape(httpOptions).then(onSuccess, onError);
+    await onStart();
     await lavine(getWorker, concurrency);
+    await onFinish();
 })();
