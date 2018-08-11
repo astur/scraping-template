3922f1cebb626213d40d6668934cce80c896c63b
astur <astur@yandex.ru>
2018-08-11 06:35:05 +0300

    db-related configs

--- conf.js
+++ conf.js
@@ -2,4 +2,17 @@ const conf = {};
 
 conf.concurrency = 20;
 
+conf.id = 'test';
+
+conf.save = {
+    valid: `data_${conf.id}`,
+    errors: `errors_${conf.id}`,
+    index: 'url',
+};
+conf.queue = {
+    name: `mq_${conf.id}`,
+    strict: true,
+    tries: 1,
+};
+
 module.exports = conf;
