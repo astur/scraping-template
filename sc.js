0a39fb65f47ecd09610ff86fa24e9c8382634384
astur <astur@yandex.ru>
2018-08-12 21:37:02 +0300

    formatted summary

--- sc.js
+++ sc.js
@@ -1,3 +1,6 @@
+const {id} = require('./conf');
+const startDt = Date.now();
+
 const {collect, summary} = require('summary-collector')({
     counters: [
         'bytesSent',
@@ -12,4 +15,32 @@ const {collect, summary} = require('summary-collector')({
     quantile: 0.95,
 });
 
-module.exports = {collect, summary};
+const _summary = arg => {
+    const sum = summary();
+    const result = {
+        src: id,
+        startDt: new Date(startDt),
+        endDt: new Date(),
+        parseDuration: Math.ceil((Date.now() - startDt) / 1000),
+        result: arg || {status: 'ok'},
+    };
+    if(sum.requestCountSuccess > 0){
+        result.requestCountTotal = sum.requestCountSuccess + sum.requestCountError;
+        result.requestCountSuccess = sum.requestCountSuccess;
+        result.requestCountError = sum.requestCountError;
+        result.bytesSent = sum.bytesSent;
+        result.bytesReceived = sum.bytesReceived;
+        result.requestTimeMin = sum.requestTime.min;
+        result.requestTimeMax = sum.requestTime.max;
+        result.requestTimeAvg = sum.requestTime.avg;
+        result.requestTime95Percentile = sum.requestTime.quantile['0.95'];
+        result.newAds = sum.inserted;
+        result.updatedAds = sum.modified;
+        result.duplicatedAds = sum.duplicated;
+        result.successAds = sum.inserted + sum.modified + sum.duplicated;
+        result.rejectedAds = sum.errors;
+    }
+    return result;
+};
+
+module.exports = {collect, summary: _summary};
