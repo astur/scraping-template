b9ac6faa09336c2c692f3f3a68f3da776496b6e7
astur <astur@yandex.ru>
2018-08-13 00:49:15 +0300

    saver for summary

--- save.js
+++ save.js
@@ -1,1 +1,5 @@
-module.exports = require('monscr')(require('./db'), require('./conf').save);
+const {save} = require('./conf');
+const db = require('./db');
+const monscr = require('monscr');
+module.exports = monscr(db, save.data);
+module.exports.saveLog = monscr(db, save.log);
