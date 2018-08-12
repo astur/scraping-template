fff1e5ea4297b89f13502bd2203eedbb635db90e
astur <astur@yandex.ru>
2018-08-13 00:45:43 +0300

    conductor options in conf

--- conf.js
+++ conf.js
@@ -10,6 +10,13 @@ conf.mongoString = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
 conf.waitForActive = 500;
 conf.minDelay = 50;
 
+conf.maxTime = null;
+conf.maxTasks = null;
+conf.maxErrors = null;
+conf.maxTagErrors = null;
+
+conf.waitForExit = 5000;
+
 conf.httpOptions = {
     timeout: 10000,
     compressed: true,
