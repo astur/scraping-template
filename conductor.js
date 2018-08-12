c502220c97f3ebeafafba90545bfc82c1998a93b
astur <astur@yandex.ru>
2018-08-13 00:41:51 +0300

    exit signals control

--- conductor.js
+++ conductor.js
@@ -1,9 +1,35 @@
-const {maxTime, maxTasks, maxErrors, maxTagErrors} = require('./conf');
+const {maxTime, maxTasks, maxErrors, maxTagErrors, waitForExit} = require('./conf');
+const onDeath = require('death');
 const abbado = require('abbado')({
     timeout: maxTime,
     count: maxTasks,
     errorLimit: maxErrors,
     tagErrorLimit: maxTagErrors,
 });
+
+let dieing = false;
+let timer = null;
+
+const offDead = onDeath((signal, err) => {
+    if(dieing) return;
+    dieing = true;
+    abbado.stop(err ? {
+        status: 'error',
+        error: err.name,
+        message: err.message,
+    } : {
+        status: 'aborted',
+        signal,
+    });
+    timer = setTimeout(() => {
+        process.exit();
+    }, waitForExit);
+});
+
+abbado.cleanup = () => {
+    abbado.resume();
+    clearTimeout(timer);
+    offDead();
+};
 
 module.exports = abbado;
