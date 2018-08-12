1e0a98ba86d5e1866ac2d452bdc978211efa2fcd
astur <astur@yandex.ru>
2018-08-12 05:38:00 +0300

    logging for db connection error

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const {mongoString} = require('./conf');
+const log = require('cllc')(null, '%F %T');
 const mongo = require('mongodb').MongoClient;
 module.exports = mongo.connect(mongoString, {useNewUrlParser: true}).then(client => {
     if('databaseName' in client) return client;
@@ -6,5 +7,6 @@ module.exports = mongo.connect(mongoString, {useNewUrlParser: true}).then(client
     db.close = client.close.bind(client);
     return db;
 }).catch(e => {
+    log.e(e);
     process.exit(1);
 });
