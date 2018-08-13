75d8db25eac4be2506ba7c0383abb79f6d09133b
astur <astur@yandex.ru>
2018-08-13 07:48:36 +0300

    catching parse error

--- parse.js
+++ parse.js
@@ -2,22 +2,31 @@ module.exports = res => {
     const records = [];
     const urls = [];
 
-    const r = {
-        url: res.url,
-        status: res.statusCode,
-        contentType: res.headers['content-type'],
-        requestDt: new Date(res.timings.start),
-    };
-
-    if(typeof res.body === 'string'){
-        r.title = (res.body.match(/<title[^>]*>([^>]+)</i) || [])[1] || null;
-        r.length = res.body.length;
-        r.charset = res.charset;
-    } else {
-        r.data = JSON.stringify(res.body);
+    try {
+        const r = {
+            url: res.url,
+            status: res.statusCode,
+            contentType: res.headers['content-type'],
+            requestDt: new Date(res.timings.start),
+        };
+        if(typeof res.body === 'string'){
+            r.title = (res.body.match(/<title[^>]*>([^>]+)</i) || [])[1] || null;
+            r.length = res.body.length;
+            r.charset = res.charset;
+        } else {
+            r.data = JSON.stringify(res.body);
+        }
+        records.push(r);
+    } catch(e){
+        const err = new Error('Can\'t parse response');
+        err.name = 'ParseError';
+        err.cause = e;
+        err.url = res.url;
+        err.status = res.statusCode;
+        err.headers = res.headers;
+        err.bodyLength = res.body.length;
+        throw err;
     }
-
-    records.push(r);
 
     return {records, urls};
 };
