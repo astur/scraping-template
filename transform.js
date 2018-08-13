81b610d41acf72379775360bf26714656962aa38
astur <astur@yandex.ru>
2018-08-13 06:12:18 +0300

    easy transform

---
+++ transform.js
@@ -0,0 +1,6 @@
+module.exports = records => records.map(r => {
+    const errors = [];
+    // if(somethingWrong(f)) errors.push('something wrong');
+    if(errors.length) r.errors = errors; // and record will be saved to errors collection
+    return r;
+});
