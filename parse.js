b13bcfb8dc1936645705a4a9960aff5c8cab0f41
astur <astur@yandex.ru>
2018-08-13 07:34:59 +0300

    parsing request date

--- parse.js
+++ parse.js
@@ -6,6 +6,7 @@ module.exports = res => {
         url: res.url,
         status: res.statusCode,
         contentType: res.headers['content-type'],
+        requestDt: new Date(res.timings.start),
     });
 
     return {records, urls};
