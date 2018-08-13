d7a1e4eb01e8e7e47b5025569b06ae8abda4e7fc
astur <astur@yandex.ru>
2018-08-13 07:35:21 +0300

    easy parsing request body

--- parse.js
+++ parse.js
@@ -7,6 +7,9 @@ module.exports = res => {
         status: res.statusCode,
         contentType: res.headers['content-type'],
         requestDt: new Date(res.timings.start),
+        data: typeof res.body === 'string' ?
+            (res.body.match(/<title[^>]*>([^>]+)</i) || [])[1] || null :
+            JSON.stringify(res.body),
     });
 
     return {records, urls};
