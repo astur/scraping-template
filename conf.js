9d190c145137c556147b3eb949839a5871896053
astur <astur@yandex.ru>
2018-08-15 09:04:15 +0300

    env vars in config

--- conf.js
+++ conf.js
@@ -1,30 +1,30 @@
 require('dotenv').config();
 const conf = {};
 
-conf.concurrency = 1;
+conf.concurrency = process.env.CONCURRENCY || 1;
 
 conf.id = 'test';
-conf.index = 'url';
+conf.index = (process.env.SCRAPE_SAVE_INDEX || 'url').split(/[.|,:]/);
 
 conf.targets = process.argv.slice(2);
 
+conf.proxyList = null;
+
 conf.mongoString = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
 
-conf.waitForActive = 50;
-conf.minDelay = null;
+conf.waitForActive = +process.env.WAIT_FOR_ACTIVE || 50;
+conf.minDelay = +process.env.SCRAPE_MIN_DELAY || null;
 
-conf.maxTime = null;
-conf.maxTasks = null;
-conf.maxErrors = null;
-conf.maxTagErrors = null;
+conf.maxTime = +process.env.SCRAPE_MAX_TIME || null;
+conf.maxTasks = +process.env.SCRAPE_MAX_TASKS || null;
+conf.maxErrors = +process.env.SCRAPE_MAX_ERRORS || null;
+conf.maxTagErrors = +process.env.SCRAPE_MAX_TAG_ERRORS || null;
 
-conf.waitForExit = 5000;
+conf.waitForExit = +process.env.WAIT_FOR_EXIT || 5000;
 
 conf.httpOptions = {
-    timeout: 10000,
+    timeout: +process.env.HTTP_TIMEOUT || 10000,
     compressed: true,
 };
-
-conf.proxyList = null;
 
 module.exports = conf;
