c7afbac2ba926b18a417bc3710334f8fe4938942
astur <astur@yandex.ru>
2018-08-15 09:44:06 +0300

    easy cli keys

--- conf.js
+++ conf.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const conf = {};
+const cli = require('oopt')('c:T:t:n:e:E:p:P:f:i:');
 
-conf.concurrency = process.env.CONCURRENCY || 1;
+conf.concurrency = +cli.c || process.env.CONCURRENCY || 1;
 
-conf.id = 'test';
-conf.index = (process.env.SCRAPE_SAVE_INDEX || 'url').split(/[.|,:]/);
+conf.id = cli.i || 'test';
+conf.index = (cli.I || process.env.SCRAPE_SAVE_INDEX || 'url').split(/[.|,:]/);
 
-conf.targets = process.argv.slice(2);
+conf.targets = cli._;
 
 conf.proxyList = null;
 
@@ -15,15 +16,15 @@ conf.mongoString = process.env.MONGO_URI || 'mongodb://localhost:27017/test';
 conf.waitForActive = +process.env.WAIT_FOR_ACTIVE || 50;
 conf.minDelay = +process.env.SCRAPE_MIN_DELAY || null;
 
-conf.maxTime = +process.env.SCRAPE_MAX_TIME || null;
-conf.maxTasks = +process.env.SCRAPE_MAX_TASKS || null;
-conf.maxErrors = +process.env.SCRAPE_MAX_ERRORS || null;
-conf.maxTagErrors = +process.env.SCRAPE_MAX_TAG_ERRORS || null;
+conf.maxTime = +cli.t || +process.env.SCRAPE_MAX_TIME || null;
+conf.maxTasks = +cli.n || +process.env.SCRAPE_MAX_TASKS || null;
+conf.maxErrors = +cli.e || +process.env.SCRAPE_MAX_ERRORS || null;
+conf.maxTagErrors = +cli.E || +process.env.SCRAPE_MAX_TAG_ERRORS || null;
 
 conf.waitForExit = +process.env.WAIT_FOR_EXIT || 5000;
 
 conf.httpOptions = {
-    timeout: +process.env.HTTP_TIMEOUT || 10000,
+    timeout: +cli.T || +process.env.HTTP_TIMEOUT || 10000,
     compressed: true,
 };
 
